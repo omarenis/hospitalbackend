c4e9b5b06deb8f6560d6c37a67172066b63f1ee0
omarenis <omar.triki@enis.tn>
2021-08-09 10:19:16 +0100

    version ready to be deployed

--- backend/settings.py
+++ backend/settings.py
@@ -79,6 +79,11 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
 
 ROOT_URLCONF = 'backend.urls'
 
@@ -99,11 +104,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'backend.wsgi.application'
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
-}
+
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
