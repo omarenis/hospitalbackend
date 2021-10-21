f0d6cc3be4d96354168fb614d88feb6b1345dbab
omarenis <omar.triki@enis.tn>
2021-10-21 02:53:19 +0100

    change the urls to /api

--- backend/urls.py
+++ backend/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import include, path
 
 
 def home():
-    return "hello world"
+    return HttpResponse(b"hello world")
 
 
 urlpatterns = [
